cd53c2dcb1d80400093f78b5a03a6c7e2b3a5f9e
Butch Davis <butch.davis@kadosh3.com>
2023-03-30 11:26:27 -0400

    Initial Commit

---
+++ ProjectorAutomation/ProjectorAutomationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectorAutomationApp.swift
+//  ProjectorAutomation
+//
+//  Created by Butch Davis on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjectorAutomationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
