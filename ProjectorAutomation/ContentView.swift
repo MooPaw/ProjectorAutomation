e7d83a5385bd6797c6babe5b0a404fcb2675f619
Butch Davis <butch.davis@kadosh3.com>
2023-04-01 11:28:10 -0400

    Build 1 of 2023.0.0 (initial commit)

--- ProjectorAutomation/ContentView.swift
+++ ProjectorAutomation/ContentView.swift
@@ -10,10 +10,24 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "car.circle")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Projector Automation")
+            if #available(iOS 15.0, *) {
+                VStack {
+                    resetCache()
+                    refreshPlan()
+                    projectorHelp()
+                }
+                .buttonStyle(.bordered)
+            } else {
+                VStack {
+                    resetCache()
+                    refreshPlan()
+                    projectorHelp()
+                }
+            }
         }
         .padding()
     }
@@ -22,5 +36,51 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct resetCache: View {
+    @Environment(\.openURL) private var openURL
+
+    var body: some View {
+        Button {
+            let url = URL (string: "projector://reset/cache")!
+            if(UIApplication.shared.canOpenURL(url)) {
+                UIApplication.shared.open(url)
+            }
+            
+        } label: {
+            Label("Reset Cache", systemImage: "digitalcrown.press")
+        }
+    }
+}
+
+struct refreshPlan: View {
+    @Environment(\.openURL) private var openURL
+
+    var body: some View {
+        Button {
+            let url = URL (string: "projector://refresh/current-plan")!
+            if(UIApplication.shared.canOpenURL(url)) {
+                UIApplication.shared.open(url)
+            }
+
+        } label: {
+            Label("Refresh Plan", systemImage: "arrow.clockwise.circle")
+        }
+    }
+}
+
+struct projectorHelp: View {
+    @Environment(\.openURL) private var openURL
+
+    var body: some View {
+        Button {
+            if let url = URL(string: "https://worshipstorm.zendesk.com/hc/en-us") {
+                openURL(url)
+            }
+        } label: {
+            Label("Projector Help", systemImage: "questionmark.circle")
+        }
     }
 }
